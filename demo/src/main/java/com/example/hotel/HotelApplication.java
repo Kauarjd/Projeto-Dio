bf592d27ddaa1128f7eaeeb77c340efa0f68b512
Kauarjd <kauarochad0123@gmail.com>
2025-02-27 17:12:15 -0300

    Resolvendo conflitos

--- demo/src/main/java/com/example/hotel/HotelApplication.java
+++ demo/src/main/java/com/example/hotel/HotelApplication.java
@@ -1,8 +1,17 @@
 package com.example.hotel;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hotel Booking API",
+                version = "1.0",
+                description = "API para agendamento de hotel"
+        )
+)
 @SpringBootApplication
 public class HotelApplication {
     public static void main(String[] args) {
